7025fe932ab69a4605eb1eb4e08ef53b2d6f7c81
John Grishin <r@exah.ru>
2018-10-15 00:47:40 +0300

    .

---
+++ README.md
@@ -0,0 +1,19 @@
+# Utils
+
+> Small set of function that helps me everyday
+
+
+## 📦 Install
+
+```sh
+$ yarn add @exah/utils
+```
+
+
+## 📖 [API](./docs/api.md)
+
+## Related
+
+---
+
+MIT © [John Grishin](http://johngrish.in)
---
+++ babel.config.js
@@ -0,0 +1,18 @@
+const IS_TEST = process.env.NODE_ENV === 'test'
+const IS_ESM = !IS_TEST && process.env.MODULES !== 'cjs'
+
+module.exports = {
+  'presets': [
+    '@babel/preset-flow',
+    [
+      '@babel/preset-env', {
+        modules: IS_ESM ? false : 'commonjs',
+        targets: IS_TEST ? { node: true } : {},
+        loose: true
+      }
+    ]
+  ],
+  'plugins': [
+    [ '@babel/plugin-proposal-object-rest-spread', { useBuiltIns: true, loose: true } ]
+  ]
+}
